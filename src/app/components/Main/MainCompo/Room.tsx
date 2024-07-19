140b792bf737c06b7bab29fa818b8c664b6740f2
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-07-19 12:53:34 +0900

    客室状況の色を反映する記述のリファクタリング

--- src/app/components/Main/MainCompo/Room.tsx
+++ src/app/components/Main/MainCompo/Room.tsx
@@ -1,35 +1,41 @@
-"use client"
+"use client";
 
-import { useAppDispatch, useAppSelector } from '@/app/features/Redux/hooks';
-import { openModal } from '@/app/features/Redux/modal/modalSlice';
-import React from 'react'
+import { useAppDispatch, useAppSelector } from "@/app/features/Redux/hooks";
+import { openModal } from "@/app/features/Redux/modal/modalSlice";
+import React from "react";
 
 interface RoomProps {
-    id: number;
-    roomNumber: string;
-    roomState: string;
+  id: number;
+  roomNumber: string;
+  roomState: string;
 }
-
 
 const Room: React.FC<RoomProps> = (props) => {
-    const dispatch = useAppDispatch();
-    const {id, roomNumber,roomState} = props;
-    
+  const dispatch = useAppDispatch();
+  const { id, roomNumber, roomState } = props;
+
+  let color = "";
+  if (roomState === "white") {
+    color = "bg-white";
+  } else if (roomState === "red") {
+    color = "bg-red-400";
+  } else if (roomState === "blue") {
+    color = "bg-blue-400";
+  } else if (roomState === "green") {
+    color = "bg-green-400";
+  } else if (roomState === "gray") {
+    color = "bg-gray-400";
+  }
+
   return (
     <div
-    key={id}
-    className={
-      roomState === "white"
-         ? `py-6 px-16 text-2xl font-semibold  bg-${roomState} border-black flex justify-center border-2 `
-         : `py-6 px-16 text-2xl font-semibold bg-${roomState}-500 flex justify-center  border-2 border-black `
-     }
-     onClick={()=>dispatch(openModal(roomNumber))}
-  >
-    <div>
-    {roomNumber}
+      key={id}
+      className={`${color} py-6 px-16 text-2xl font-semibold   border-black flex justify-center border-2`}
+      onClick={() => dispatch(openModal(roomNumber))}
+    >
+      <div>{roomNumber}</div>
     </div>
-  </div>
-)
-}
+  );
+};
 
-export default Room
+export default Room;
