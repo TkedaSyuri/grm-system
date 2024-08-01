ab48fc0c5cceebce30c31c261f38cd20e9572db3
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-08-01 14:49:18 +0900

    不要な記述を削除

--- src/app/features/Redux/floor/floorSlice.ts
+++ src/app/features/Redux/floor/floorSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction,createAsyncThunk} from "@reduxjs/toolkit";
+import { createSlice, PayloadAction} from "@reduxjs/toolkit";
 import { FloorData, room} from "../../Types";
 
 const initialState: FloorData= {
