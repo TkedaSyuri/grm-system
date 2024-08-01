fcf6867f791442d2e28b32cc428f0d684061c896
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-08-01 14:50:20 +0900

    createAsyncを使って部屋のroomStateを変更する記述を追加

--- src/app/features/Redux/Modal/Modal.tsx
+++ src/app/features/Redux/Modal/Modal.tsx
@@ -1,24 +1,65 @@
+
 import React from "react";
 import { useAppDispatch, useAppSelector } from "@/app/features/Redux/hooks";
-import { closeModal} from "./modalSlice";
+import { closeModal, fetchAsyncUpdate } from "./modalSlice";
 
-const Modal:React.FC = () => {
+const Modal: React.FC = () => {
+  const { roomNumber, roomId } = useAppSelector((state) => state.modal);
+  const dispatch = useAppDispatch();
 
-  const { roomNumber } = useAppSelector((state) => state.modal);
-  const dispatch = useAppDispatch()
+  //roomのroomStateの状態を変更
+  const renewState= async(state: string,roomId: number)=>{
+    await dispatch(fetchAsyncUpdate({state,roomId}))
+    dispatch(closeModal())
+  }
+
   return (
     <div>
-      <div className="bg-black text-lg fixed inset-0 bg-opacity-50 flex justify-center items-center">
+      <div className="bg-black text-lg fixed inset-0 bg-opacity-50 flex justify-center items-center cursor-default  ">
         <div className="bg-white p-10 ">
-          <div className="text-4xl underline font-semibold">部屋番号 {roomNumber}</div>
-          <div className="text-4xl my-4 flex gap-3  ">
-            <div className="border-  border-black px-6">空室</div>
-            <div className="border-4 border-black px-3  bg-red-400">在室中</div>
-            <div className="border-4 border-black px-3  bg-blue-400">清掃中</div>
-            <div className="border-4 border-black bg-green-400">清掃完了</div>
-            <div className="border-4 border-black bg-gray-400">清掃不要</div>
+          <div className="text-4xl underline font-semibold">
+            部屋番号 {roomNumber}
           </div>
-          <div className="text-2xl flex justify-end" onClick={()=>dispatch(closeModal())}>❌閉じる</div>
+          <div className="text-4xl my-4 flex gap-3  ">
+            <div
+             className="border-4 border-black px-6 hover:bg-slate-100"
+             onClick={()=>renewState("white",roomId)}
+             >
+              空室
+            </div>
+            <div
+              className="border-4 border-black px-3  bg-red-400  hover:bg-red-600"
+              onClick={()=>renewState("red",roomId)}
+              >
+              在室中
+            </div>
+            <div
+              className="border-4 border-black px-3  bg-blue-400  hover:bg-blue-600 "
+              onClick={()=>renewState("blue",roomId)}
+
+            >
+              清掃中
+            </div>
+            <div
+              className="border-4 border-black bg-green-400 hover:bg-green-600  "
+              onClick={()=>renewState("green",roomId)}
+
+            >
+              清掃完了
+            </div>
+            <div className="border-4 border-black bg-gray-400  hover:bg-gray-600 "
+             onClick={()=>renewState("gray",roomId)}
+
+            >
+              清掃不要
+            </div>
+          </div>
+          <div
+            className="text-2xl flex justify-end"
+            onClick={() => dispatch(closeModal())}
+          >
+            ❌閉じる
+          </div>
         </div>
       </div>
     </div>
--- src/app/features/Redux/Modal/ModalSlice.ts
+++ src/app/features/Redux/Modal/ModalSlice.ts
@@ -1,27 +1,52 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface ModalState {
-  isOpen: boolean;
-  roomNumber: string; 
+import { createSlice, PayloadAction,createAsyncThunk } from "@reduxjs/toolkit";
+import { ModalState } from "../../Types";
+
+const apiUrl = "http://localhost:8080/editRoomState/"
+
+interface UpdateRoomStatePayload {
+  state: string;
+  roomId: number;
 }
+
+export const fetchAsyncUpdate =createAsyncThunk("room/put",async ({ state, roomId }: UpdateRoomStatePayload,thunkAPI)=>{
+  try {
+    const response =  await fetch(`${apiUrl}${roomId}`,
+       {
+       method: "PUT",
+       headers: { "Content-Type": "application/json" },
+       body: JSON.stringify({ roomState: `${state}` }),
+     });
+     if (!response.ok) {
+      return thunkAPI.rejectWithValue('Failed to update room state');
+    }
+     return response.json()
+   } catch (err) {
+     console.log(err);
+   }
+
+}) 
 
 const initialState: ModalState = {
   isOpen: false,
   roomNumber: "", 
+  roomId:0
 };
+
 
 const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    openModal: (state, action: PayloadAction<string>) => {
+    openModal: (state, action: PayloadAction<{roomNumber: string,roomId: number}>) => {
       state.isOpen = !state.isOpen;
-      state.roomNumber = action.payload;
+      state.roomNumber = action.payload.roomNumber;
+      state.roomId = action.payload.roomId;
     },
     closeModal: (state) => {
       state.isOpen = !state.isOpen;
     },
-  },
+  }
 });
 
 export const { openModal, closeModal} = modalSlice.actions;
