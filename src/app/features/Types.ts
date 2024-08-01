61b8c813baa2fc3f022221cfe91eb3b43009992f
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-08-01 14:48:23 +0900

    interfaceを追加

--- src/app/features/Types.ts
+++ src/app/features/Types.ts
@@ -1,10 +1,18 @@
+//floorSliceで使用するType
 export interface room {
   id: number;
-  roomNumber: string;
+  roomNumber: string ;
   roomState: string;
 }
 
 export interface FloorData {
   floorData: room[];
   floorNumber: string;
+}
+
+//ModalSliceで使用するType
+export interface ModalState {
+  isOpen: boolean;
+  roomNumber: string; 
+  roomId: number;
 }
