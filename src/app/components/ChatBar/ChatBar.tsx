99f4af56903dbca3699464026dc57f02923297ae
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-08-05 13:00:30 +0900

    d

--- src/app/components/ChatBar/ChatBar.tsx
+++ src/app/components/ChatBar/ChatBar.tsx
@@ -1,25 +1,30 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import io from "socket.io-client";
 
 const socket = io(`${process.env.NEXT_PUBLIC_API_BASEURL}`);
 
-interface chatMessage {
+interface ChatMessage {
   message: string;
 }
 
 const ChatBar: React.FC = () => {
-  const [chatList, setChatList] = useState<Array<chatMessage>>([]);
+  const [chatList, setChatList] = useState<Array<ChatMessage>>([]);
   const messageRef = useRef<HTMLInputElement | null>(null);
 
-  //サーバーからデータを受け取る。
-  socket.on("recieved_message", (data) => {
-    console.log(data);
-    setChatList([...chatList, data]);
-  });
+  useEffect(() => {
+    // サーバーからデータを受け取る。
+    socket.on("received_message", (data: ChatMessage) => {
+      setChatList((prevChatList) => [...prevChatList, data]);
+    });
 
-  //チャットのメッセージを送信
+    return () => {
+      socket.off("received_message");
+    };
+  }, []);
+
+  // チャットのメッセージを送信
   const submitMessage = () => {
     if (messageRef.current?.value) {
       const message = messageRef.current?.value.trim();
@@ -29,40 +34,37 @@ const ChatBar: React.FC = () => {
   };
 
   const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    const text: string = e.key;
-    if (text === "Enter") {
+    if (e.key === "Enter") {
       submitMessage();
     }
   };
 
   return (
-    <div className=" w-96 h-full border-2 border-black flex flex-col ">
-      <div className="  py-2 r bg-green-500 border-b-2 border-black flex justify-center">
+    <div className="w-96 h-full border-2 border-black flex flex-col">
+      <div className="py-2 bg-green-500 border-b-2 border-black flex justify-center">
         連絡チャット
       </div>
-      <div className="break-words flex-grow  bg-white">
+      <div className="flex-grow bg-white overflow-y-auto">
         {chatList.map((chat, index) => (
-          <div key={index}>
-            <p className=" border-b border-black">{chat.message}</p>
+          <div key={index} className="break-words border-b border-black">
+            <p>{chat.message}</p>
           </div>
         ))}
       </div>
-      <div className="border-t-2">
-        <div className="flex justify-start">
-          <input
-            type="text"
-            placeholder="チャットを入力"
-            className="outline-none w-full"
-            ref={messageRef}
-            onKeyDown={handleEnter}
-          />
-          <button
-            onClick={() => submitMessage()}
-            className=" px-1 bg-green-500 border-2 border-gray-500"
-          >
-            📤
-          </button>
-        </div>
+      <div className="border-t-2 p-2 flex">
+        <input
+          type="text"
+          placeholder="チャットを入力"
+          className="outline-none w-full"
+          ref={messageRef}
+          onKeyDown={handleEnter}
+        />
+        <button
+          onClick={submitMessage}
+          className="px-1 bg-green-500 border-2 border-gray-500"
+        >
+          📤
+        </button>
       </div>
     </div>
   );
