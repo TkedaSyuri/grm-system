13d1813c0c658c5e3542e17117a6802bccf42346
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-08-06 20:01:54 +0900

    データ取得するapiのurlを変更

--- src/app/features/hooks/useGetData.ts
+++ src/app/features/hooks/useGetData.ts
@@ -7,11 +7,15 @@ async function fetcher(key: string) {
   return fetch(key).then((res) => res.json());
 }
 
+
+
+
+
 //フロアのデータを取得するカスタムフック
 export const useGetData = (floorNumber: string) => {
   const dispatch = useAppDispatch();
   const { data, isLoading, error } = useSWR(
-    `${process.env.NEXT_PUBLIC_API_BASEURL}/getFloorData/${floorNumber}`,
+  `http://localhost:10000/getFloorData/${floorNumber}`,
     fetcher,
     {
       refreshInterval: 1000,
