fe32474fca6d36b1127e0488a34a377097c9670f
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-08-16 16:45:10 +0900

    インターバル3秒延長

--- src/app/features/hooks/useGetData.ts
+++ src/app/features/hooks/useGetData.ts
@@ -16,7 +16,7 @@ export const useGetData = (floorNumber: string) => {
     `${process.env.NEXT_PUBLIC_API_BASEURL}/getFloorData/${floorNumber}` ,
     fetcher,
     {
-      refreshInterval: 1000,
+      refreshInterval: 3000,
     }
   );
 
