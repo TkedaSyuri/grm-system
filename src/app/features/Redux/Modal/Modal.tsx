5f7eb4817af47d7aa9cd81a7ea37339de5a8d98d
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-09-10 10:34:42 +0900

    連泊の機能を追加

--- src/app/features/Redux/Modal/Modal.tsx
+++ src/app/features/Redux/Modal/Modal.tsx
@@ -1,33 +1,70 @@
-
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
-import { closeModal} from "./ModalSlice";
+import { closeModal, fetchAsyncToggleConsec } from "./ModalSlice";
 import StateBtn from "./StateBtn";
+import { IoMdClose } from "react-icons/io";
 
 const Modal: React.FC = () => {
-  const { roomNumber} = useAppSelector((state) => state.modal);
+  const { roomNumber,roomId } = useAppSelector((state) => state.modal);
   const dispatch = useAppDispatch();
+
+const handleIsConsec = async( roomId: number)=>{
+    await dispatch(fetchAsyncToggleConsec(roomId));
+    dispatch(closeModal());
+}
 
 
   return (
     <div>
-      <div className="bg-black text-lg fixed inset-0 bg-opacity-50 flex justify-center items-center cursor-default  ">
+      <div className="bg-black text-lg fixed inset-0 bg-opacity-50 z-50 flex justify-center items-center cursor-default  ">
         <div className="bg-white p-10 ">
           <div className="text-4xl underline font-semibold">
             部屋番号 {roomNumber}
           </div>
           <div className="text-4xl my-4 flex gap-3  ">
-            <StateBtn color="white" text="空室" bg="bg-white" bgHover="hover:bg-slate-100"/>
-            <StateBtn color="red" text="在室中" bg="bg-red-400" bgHover="hover:bg-red-600"/>
-            <StateBtn color="blue" text="清掃中" bg="bg-blue-400"  bgHover="hover:bg-blue-600"/>
-            <StateBtn color="green" text="清掃完了" bg="bg-green-400"  bgHover="hover:bg-green-600"/>
-            <StateBtn color="gray" text="清掃不要" bg="bg-gray-400"  bgHover="hover:bg-gray-600"/>
+            <StateBtn
+              state="white"
+              text="空室"
+              bg="bg-white"
+              bgHover="hover:bg-slate-100"
+            />
+            <StateBtn
+              state="red"
+              text="在室中"
+              bg="bg-red-400"
+              bgHover="hover:bg-red-600"
+            />
+            <StateBtn
+              state="blue"
+              text="清掃中"
+              bg="bg-blue-400"
+              bgHover="hover:bg-blue-600"
+            />
+            <StateBtn
+              state="green"
+              text="清掃完了"
+              bg="bg-green-400"
+              bgHover="hover:bg-green-600"
+            />
+            <StateBtn
+              state="gray"
+              text="清掃不要"
+              bg="bg-gray-400"
+              bgHover="hover:bg-gray-600"
+            />
+            <div
+              className="border-4 border-black px-6 bg-yellow-400 hover:bg-yellow-500"
+              onClick={()=>handleIsConsec(roomId)}
+            >
+              連泊
+            </div>
           </div>
           <div
-            className="text-2xl flex justify-end"
+            className="text-4xl font-normal flex justify-end items-center"
             onClick={() => dispatch(closeModal())}
           >
-            ❌閉じる
+            <IoMdClose className="text-red-600" />
+            <p>閉じる</p>
           </div>
         </div>
       </div>
--- src/app/features/Redux/Modal/ModalSlice.ts
+++ src/app/features/Redux/Modal/ModalSlice.ts
@@ -1,40 +1,66 @@
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+import { ModalState, UpdateRoomState } from "../../Types";
 
-import { createSlice, PayloadAction,createAsyncThunk } from "@reduxjs/toolkit";
-import { ModalState, UpdateRoomStatePayload } from "../../Types";
-
-const apiUrl = `${process.env.NEXT_PUBLIC_API_BASEURL}/editRoomState/`
+const apiUrl = `${process.env.NEXT_PUBLIC_API_BASEURL}`;
 
 
-export const fetchAsyncUpdate =createAsyncThunk("room/put",async ({ state, roomId }: UpdateRoomStatePayload,thunkAPI)=>{
-  try {
-    const response =  await fetch(`${apiUrl}${roomId}`,
-       {
-       method: "PUT",
-       headers: { "Content-Type": "application/json" },
-       body: JSON.stringify({ roomState: `${state}` }),
-     });
-     if (!response.ok) {
-      return thunkAPI.rejectWithValue('部屋のステータス変更に失敗しました。');
+
+//roomの状態を変更するapi
+export const fetchAsyncUpdate = createAsyncThunk(
+  "room/put",
+  async ({ state, roomId }: UpdateRoomState, thunkAPI) => {
+    try {
+      const response = await fetch(`${apiUrl}/editRoomState/${roomId}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ roomState: `${state}` }),
+      });
+      if (!response.ok) {
+        return thunkAPI.rejectWithValue("部屋のステータス変更に失敗しました。");
+      }
+      return response.json();
+    } catch (err) {
+      console.log(err);
     }
-     return response.json()
-   } catch (err) {
-     console.log(err);
-   }
+  }
+);
 
-}) 
+
+export const fetchAsyncToggleConsec = createAsyncThunk(
+  "isConsec/toggle",
+  async (roomId: number, thunkAPI) => {
+    try {
+      const response = await fetch(`${apiUrl}/isConsec/${roomId}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ is_ConsecRoom: true }),
+      });
+      if (!response.ok) {
+        return thunkAPI.rejectWithValue("isConsecの変更に失敗しました。");
+      }
+      return response.json();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+
+
 
 const initialState: ModalState = {
   isOpen: false,
-  roomNumber: "", 
-  roomId:0
+  roomNumber: "",
+  roomId: 0,
 };
-
 
 const modalSlice = createSlice({
   name: "modal",
   initialState,
   reducers: {
-    openModal: (state, action: PayloadAction<{roomNumber: string,roomId: number}>) => {
+    openModal: (
+      state,
+      action: PayloadAction<{ roomNumber: string; roomId: number }>
+    ) => {
       state.isOpen = !state.isOpen;
       state.roomNumber = action.payload.roomNumber;
       state.roomId = action.payload.roomId;
@@ -42,8 +68,8 @@ const modalSlice = createSlice({
     closeModal: (state) => {
       state.isOpen = !state.isOpen;
     },
-  }
+  },
 });
 
-export const { openModal, closeModal} = modalSlice.actions;
+export const { openModal, closeModal } = modalSlice.actions;
 export default modalSlice.reducer;
