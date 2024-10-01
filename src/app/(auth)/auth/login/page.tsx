6fb34e0a6b291478c57ce7f6444cc349841b6cb3
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-10-01 16:14:44 +0900

    認証ページ追加しました

---
+++ src/app/(auth)/auth/login/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { loginFormSchema } from "@/app/features/Auth/utils/AuthFormSchema";
+import Link from "next/link";
+import {
+  fetchAsyncLogin,
+  fetchAsyncStaff,
+} from "@/app/features/Redux/Auth/AuthSlice";
+import { useAppDispatch} from "@/app/features/Redux/hooks";
+
+interface LoginForm {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const LoginPage = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<LoginForm> = async (data: LoginForm) => {
+    const { email, password } = data;
+    try {
+      const result = await dispatch(fetchAsyncLogin({ email, password }));
+      if (fetchAsyncLogin.fulfilled.match(result)) {
+        await dispatch(fetchAsyncStaff());
+      }
+      router.push("/");
+    } catch (err) {
+      console.error("エラーが発生しました:", err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <div className="pb-3">
+          <h1 className=" font-semibold text-center border-b border-green-600 ">
+            ログイン
+          </h1>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="email" className="block text-sm font-semibold mb-2">
+              メールアドレス
+            </label>
+            <input
+              id="email"
+              type="email"
+              {...register("email")}
+              className="w-full p-2 border border-gray-300 rounded-md mb-4 outline-none"
+              autoComplete="mail"
+            />
+            <p className="text-red-500 text-xs mb-4">
+              {errors.email?.message as React.ReactNode}
+            </p>
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold mb-2"
+            >
+              パスワード
+            </label>
+            <input
+              id="password"
+              type="password"
+              {...register("password")}
+              className="w-full p-2 border border-gray-300 rounded-md mb-4 outline-none"
+              autoComplete="new-password"
+            />
+            <div className="text-end">
+              <Link
+                href="/auth/signup"
+                className="text-blue-600 hover:text-green-400 duration-300"
+              >
+                新規登録はこちら
+              </Link>
+            </div>
+
+            <p className="text-red-500 text-xs mb-4">
+              {errors.password?.message as React.ReactNode}
+            </p>
+          </div>
+          <button
+            type="submit"
+            className="w-full p-2 bg-green-700 text-white font-bold rounded-md hover:bg-green-500 transition duration-300 mt-4"
+          >
+            送信
+          </button>
+          <div className="pt-3 text-center">
+            <Link href="/">キャンセル</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/(auth)/auth/signup/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { signupFormSchema } from "@/app/features/Auth/utils/AuthFormSchema";
+import { fetchAsyncSingup } from "@/app/features/Redux/Auth/AuthSlice";
+import { useAppDispatch } from "@/app/features/Redux/hooks";
+
+export interface SignUpForm {
+  staffName: string;
+  email: string;
+  password: string;
+}
+
+
+const SignUpPage = () => {
+  const dispatch = useAppDispatch()
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpForm>({
+    mode: "onChange",
+    resolver: zodResolver(signupFormSchema),defaultValues: {
+      staffName: "",
+      email: "",
+      password: "",
+    }
+  });
+
+  const onSubmit: SubmitHandler<SignUpForm> = async (data: SignUpForm) => {
+    const {staffName,email,password} = data;
+    await dispatch(fetchAsyncSingup({staffName,email,password}))
+    router.push("/auth/login");
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200 ">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md ">
+        <div className="pb-3">
+          <h1 className=" font-semibold text-center border-b border-green-600 ">
+            新規登録
+          </h1>
+        </div>
+        <form  onSubmit={handleSubmit(onSubmit)} >
+          <div>
+            <label htmlFor="name" className="block text-sm font-semibold mb-2">
+              名前
+            </label>
+            <input
+              id="name"
+              type="text"
+              {...register("staffName")}
+              className="w-full p-2 border border-gray-300 rounded-md mb-4 outline-none"
+              autoComplete="name"
+            />
+            <p className="text-red-500 text-xs mb-4">
+              {errors.staffName?.message as React.ReactNode}
+            </p>
+          </div>
+          <div>
+            <label htmlFor="email" className="block text-sm font-semibold mb-2">
+              メールアドレス
+            </label>
+            <input
+              id="email"
+              type="email"
+              {...register("email")}
+              className="w-full p-2 border border-gray-300 rounded-md mb-4 outline-none"
+              autoComplete="mail"
+            />
+            <p className="text-red-500 text-xs mb-4">
+              {errors.email?.message as React.ReactNode}
+            </p>
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold mb-2"
+            >
+              パスワード
+            </label>
+            <input
+              id="password"
+              type="password"
+              {...register("password")}
+              className="w-full p-2 border border-gray-300 rounded-md mb-4 outline-none"
+              autoComplete="new-password"
+            />
+            <p className="text-red-500 text-xs mb-4">
+              {errors.password?.message as React.ReactNode}
+            </p>
+          </div>
+          <button
+            type="submit"
+            className="w-full p-2 bg-green-700 text-white font-bold rounded-md hover:bg-green-500  transition duration-300 mt-4"
+          >
+            送信
+          </button>
+          <div className="pt-3 text-center">
+            <Link href="/" >キャンセル</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
