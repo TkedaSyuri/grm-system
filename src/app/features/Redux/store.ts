a1fe3afb059a8ea5ffb934d73e189b1cd6f295ae
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-10-15 13:31:35 +0900

    toggleSliceを追加

--- src/app/features/Redux/store.ts
+++ src/app/features/Redux/store.ts
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import floorReducer from "./floor/floorSlice";
-import modalReducer from "./modal/modalSlice";
 import authReducer from "./auth/authSlice";
 import taskReducer from "./task/taskSlice"
+import toggleReducer from "./toggle/toggleSlice"
 
 export const store = configureStore({
   reducer: {
     floor:floorReducer,
-    modal: modalReducer,
     staff:authReducer,
-    task: taskReducer
+    task: taskReducer,
+    toggle: toggleReducer,
   },
 });
 
---
+++ src/app/features/Redux/toggle/toggleSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface initialToggleState {
+  isModalOpen: boolean;
+  isBarOpen: boolean;
+  isCompletedTaskOpen: boolean;
+  roomNumber: string;
+  roomId: number;
+}
+
+const initialState: initialToggleState = {
+  isModalOpen: false,
+  isBarOpen: false,
+  isCompletedTaskOpen: false,
+  roomNumber: "",
+  roomId: 0,
+};
+
+const toggleSlice = createSlice({
+  name: "toggle",
+  initialState,
+  reducers: {
+    openModal: (
+      state,
+      action: PayloadAction<{ roomNumber: string; roomId: number }>
+    ) => {
+      state.isModalOpen = !state.isModalOpen;
+      state.roomNumber = action.payload.roomNumber;
+      state.roomId = action.payload.roomId;
+    },
+    closeModal: (state) => {
+      state.isModalOpen = !state.isModalOpen;
+    },
+    openTaskBar: (state) => {
+      state.isBarOpen = false;
+    },
+    openChatBar: (state) => {
+      state.isBarOpen = true;
+    },
+    toggleCompletedTask: (state) =>{
+      state.isCompletedTaskOpen = !state.isCompletedTaskOpen
+    }
+  },
+});
+
+export const { openModal, closeModal, openTaskBar,openChatBar,toggleCompletedTask} = toggleSlice.actions;
+export default toggleSlice.reducer;
