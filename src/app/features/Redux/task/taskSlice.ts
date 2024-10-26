541f61f4743d5ef6c83d4b5e952add3c671a8538
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-10-26 13:21:28 +0900

    コードの整形

--- src/app/features/Redux/task/taskSlice.ts
+++ src/app/features/Redux/task/taskSlice.ts
@@ -1,81 +1,82 @@
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import { Tasks } from "../../types";
 
-  interface CompletedTask{
+interface CompletedTask {
   id: number;
-  isCompleted: boolean
+  isCompleted: boolean;
 }
-
 
 export const fetchAsyncPostTask = createAsyncThunk(
-    "task/create",
-    async (task:string) => {
-      try {
-        const res= await fetch(`${process.env.NEXT_PUBLIC_API_BASEURL}/api/tasks/create-task`, {
+  "task/create",
+  async (task: string) => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASEURL}/api/tasks/create-task`,
+        {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({task: task})
-        });
-        return res.json();
-      } catch (err) {
-        console.log(err);
-      }
+          body: JSON.stringify({ task: task }),
+        }
+      );
+      return res.json();
+    } catch (err) {
+      console.log(err);
     }
-  );
-
-
+  }
+);
 
 export const fetchAsyncCompletedTask = createAsyncThunk(
-    "task/completedTask",
-    async ({id,isCompleted}:CompletedTask) => {
-      try {
-        const res= await fetch(`${process.env.NEXT_PUBLIC_API_BASEURL}/api/tasks/edit-completed-task/${id}`, {
+  "task/completedTask",
+  async ({ id, isCompleted }: CompletedTask) => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASEURL}/api/tasks/edit-completed-task/${id}`,
+        {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({isCompleted: !isCompleted})
-        });
-        return res.json();
-      } catch (err) {
-        console.log(err);
-      }
+          body: JSON.stringify({ isCompleted: !isCompleted }),
+        }
+      );
+      return res.json();
+    } catch (err) {
+      console.log(err);
     }
-  );
-  
-  
+  }
+);
+
 export const fetchAsyncDeleteTask = createAsyncThunk(
-    "task/delete",
-    async (id:number) => {
-      try {
-         await fetch(`${process.env.NEXT_PUBLIC_API_BASEURL}/api/tasks/delete-task/${id}`, {
+  "task/delete",
+  async (id: number) => {
+    try {
+      await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASEURL}/api/tasks/delete-task/${id}`,
+        {
           method: "DELETE",
-        });
-      } catch (err) {
-        console.log(err);
-      }
+        }
+      );
+    } catch (err) {
+      console.log(err);
     }
-  );
-  
-  
-  
+  }
+);
 
 interface InitialTaskState {
-    taskData: Tasks[]
+  taskData: Tasks[];
 }
 
-const initialState:InitialTaskState = {
-    taskData:[],
-}
+const initialState: InitialTaskState = {
+  taskData: [],
+};
 
 const taskSlice = createSlice({
-    name: "task",
-    initialState,
-    reducers: {
-      setTask: (state,action:PayloadAction<Tasks[]>) => {
-        state.taskData = action.payload;
-      },
+  name: "task",
+  initialState,
+  reducers: {
+    setTask: (state, action: PayloadAction<Tasks[]>) => {
+      state.taskData = action.payload;
     },
-  });
-  
-  export const {  setTask } = taskSlice.actions;
-  export default taskSlice.reducer;
-   
+  },
+});
+
+export const { setTask } = taskSlice.actions;
+export default taskSlice.reducer;
