41544d22cc394519bb7520b815e31ccd033215fe
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-10-30 15:14:04 +0900

    ログアウトの場所を変更しました

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -27,34 +27,33 @@ export default function Home() {
     <main className="overflow-hidden mb-0">
       <div className="mt-5 flex w-full max-w-screen-xl mx-auto ">
         <div className="w-4/12 ">
+          <div className="pb-2 text-start">
+            {staff ? (
+              <div className="text-white text-lg flex border-b w-fit items-center hover:text-green-400  ">
+                <button
+                  className=" font-semibold  duration-300 cursor-default"
+                  onClick={() => logoutStaff()}
+                >
+                  ログアウト
+                </button>
+                <LuLogOut />
+              </div>
+            ) : (
+              <Link
+                href="/auth/login"
+                className="font-semibold text-white  hover:text-green-400 duration-300 cursor-default "
+                prefetch={false}
+              >
+                <p className="border-b w-fit"> フロントスタッフはこちらへ</p>
+              </Link>
+            )}
+          </div>
+
           <SideBar />
         </div>
         <div className=" mt-12  w-8/12 ">
           <div className="text-white text-2xl">現在の日時 : {currentTime}</div>
           <div>
-            {staff ? (
-              <div className=" pb-2 ">
-                <div className="text-white  flex justify-end items-center hover:text-green-400  ">
-                  <button
-                    className=" font-semibold border-b duration-300 cursor-default"
-                    onClick={() => logoutStaff()}
-                  >
-                    ログアウト
-                  </button>
-                  <LuLogOut />
-                </div>
-              </div>
-            ) : (
-              <div className="pb-2">
-                <Link
-                  href="/auth/login"
-                  className="font-semibold text-white flex justify-end  hover:text-green-400 duration-300 cursor-default "
-                  prefetch={false}
-                >
-                  <p className="border-b "> フロントスタッフはこちらへ</p>
-                </Link>
-              </div>
-            )}
             <div>
               <StateSign />
             </div>
