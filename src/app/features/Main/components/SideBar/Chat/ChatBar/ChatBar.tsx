1c8a0e5303b617239300f9599e2334e9c326ebc0
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-10-31 13:23:54 +0900

    リファクタリングをしました

--- src/app/features/Main/components/SideBar/Chat/ChatBar/ChatBar.tsx
+++ src/app/features/Main/components/SideBar/Chat/ChatBar/ChatBar.tsx
@@ -1,13 +1,12 @@
 import { useGetChats } from "@/app/features/hooks/useGetChats";
 import {
   fetchAsyncDeleteAllMessage,
-  fetchAsyncDeleteMessage,
   fetchAsyncPostMessage,
 } from "@/app/features/Redux/chat/chatSlice";
 import { useAppDispatch, useAppSelector } from "@/app/features/Redux/hooks";
 import React, { useCallback, useRef } from "react";
 import { FiSend } from "react-icons/fi";
-import { FaTrashAlt } from "react-icons/fa";
+import ChatsList from "../ChatsList/ChatsList";
 
 const ChatBar = () => {
   const { staff } = useAppSelector((state) => state.staff);
@@ -24,13 +23,6 @@ const ChatBar = () => {
     }
   }, []);
 
-  const deleteMessage = (id: number) => {
-    const isConfirmed = window.confirm("本当にメッセージを削除しますか？");
-    if (isConfirmed) {
-      dispatch(fetchAsyncDeleteMessage(id));
-    }
-  };
-
   const deleteAllMessage = () => {
     const isConfirmed = window.confirm(
       "本当に全てのメッセージを削除しますか？"
@@ -42,35 +34,14 @@ const ChatBar = () => {
 
   return (
     <div className="flex-col">
-      <div className="h-96 bg-white overflow-auto">
-        <ul>
-        {chatData.map((chat) => (
-          <li
-            key={chat.id}
-            className="p-2 border-b border-gray-200 flex justify-between  break-words "
-          >
-            <p className="font-semibold">{chat.message}</p>
-            <div className="flex items-center">
-              <p>
-                {new Date(chat.created_at).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
-              </p>
-              {staff && (
-                <button
-                  className="p-1 ml-2 font-semibold text-sm bg-red-500 rounded-md border border-black flex-shrink-0"
-                  onClick={() => deleteMessage(chat.id)}
-                >
-                  <FaTrashAlt />
-                </button>
-              )}
-            </div>
-          </li>
-        ))}
-
-        </ul>
-      </div>
+    <div className="h-96 bg-white overflow-auto">
+      <ul>
+      {chatData.map((chat) => (      
+        <ChatsList id={chat.id}  message={chat.message} created_at={chat.created_at} />
+))}
+      </ul>
+     </div>
+      
       <div className="p-2 bg-slate-300 border-t border-black ">
         <textarea
           placeholder="チャットを入力"
@@ -87,16 +58,16 @@ const ChatBar = () => {
             <FiSend />
           </button>
           {staff && (
-          <div className="flex justify-end">
-            <button
-              className="px-5 py-1  font-semibold text-sm bg-red-600 hover:bg-red-500 border border-black flex-shrink-0 outline-none"
-              onClick={() => deleteAllMessage()}
-              title="全てのメッセージを削除"
-            >
-              リセット
-            </button>
-          </div>
-        )}
+            <div className="flex justify-end">
+              <button
+                className="px-5 py-1  font-semibold text-sm bg-red-600 hover:bg-red-500 border border-black flex-shrink-0 outline-none"
+                onClick={() => deleteAllMessage()}
+                title="全てのメッセージを削除"
+              >
+                リセット
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
---
+++ src/app/features/Main/components/SideBar/Chat/ChatsList/ChatsList.tsx
@@ -0,0 +1,54 @@
+import { fetchAsyncDeleteMessage } from "@/app/features/Redux/chat/chatSlice";
+import { useAppDispatch, useAppSelector } from "@/app/features/Redux/hooks";
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+
+interface ChatsListProps{
+    id:number;
+    message: string;
+    created_at: string
+} 
+
+
+
+const ChatsList:React.FC<ChatsListProps> = ({id,message,created_at}) => {
+  const { staff } = useAppSelector((state) => state.staff);
+
+  const dispatch = useAppDispatch();
+
+
+  const deleteMessage = (id: number) => {
+    const isConfirmed = window.confirm("本当にメッセージを削除しますか？");
+    if (isConfirmed) {
+      dispatch(fetchAsyncDeleteMessage(id));
+    }
+  };
+
+  return (
+          <li
+            key={id}
+            className="p-2 border-b border-gray-200 flex justify-between  break-words "
+          >
+            <p className="font-semibold">{message}</p>
+            <div className="flex items-center">
+              <p>
+                {new Date(created_at).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
+              {staff && (
+                <button
+                  className="p-1 ml-2 font-semibold text-sm bg-red-500 rounded-md border border-black flex-shrink-0"
+                  onClick={() => deleteMessage(id)}
+                >
+                  <FaTrashAlt />
+                </button>
+              )}
+            </div>
+          </li>
+  );
+};
+
+export default ChatsList
