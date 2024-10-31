e74a259f8084b6527397c7b5286075de075bc0da
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-10-31 12:52:23 +0900

    ファイルの名前を変更

--- src/app/features/Main/Index.ts
+++ src/app/features/Main/Index.ts
@@ -1,7 +1,7 @@
 import FloorLink from "./components/FloorLink/FloorLink";
 import FloorTable from "./components/FloorTable/FloorTable";
 import Modal from "./components/Modal/Modal";
-import Room from "./components/Room/Room";
+import Room from "./components/Rooms/Rooms";
 import SideBar from "./components/SideBar/SideBar";
 import StateSign from "./components/StateSign/StateSign";
 
