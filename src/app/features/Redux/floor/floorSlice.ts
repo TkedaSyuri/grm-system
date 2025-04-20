84ef98b199b3e589bbc06b1219f34592ea74aa15
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-04-20 18:13:15 +0900

    Sliceとapiを分離

--- src/app/features/Redux/floor/floorSlice.ts
+++ src/app/features/Redux/floor/floorSlice.ts
@@ -1,44 +1,13 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import {  Rooms, RoomState} from "../../types";
-
-export interface UpdateRoomState {
-  state: RoomState;
-  roomId: number;
-}
-
-export const fetchAsyncUpdate = createAsyncThunk(
-    "floor/put",
-    async ({ state, roomId }: UpdateRoomState, thunkAPI) => {
-      try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASEURL}/api/room/edit/room-state/${roomId}`, {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ roomState: `${state}` }),
-        });
-        if (!response.ok) {
-          return thunkAPI.rejectWithValue("部屋のステータス変更に失敗しました。");
-        }
-        return response.json();
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  );
-  
-  
-  
-
-
-
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Rooms } from "../../types";
 export interface FloorInitialState {
   floorData: Rooms[];
   floorNumber: string;
 }
 
-
-const initialState: FloorInitialState= {
-  floorData:[],
-  floorNumber: "2"
+const initialState: FloorInitialState = {
+  floorData: [],
+  floorNumber: "2",
 };
 
 const floorSlice = createSlice({
@@ -48,11 +17,11 @@ const floorSlice = createSlice({
     setFloor: (state, action: PayloadAction<Rooms[]>) => {
       state.floorData = action.payload;
     },
-    changeFloor: (state,action)=>{
+    changeFloor: (state, action) => {
       state.floorNumber = action.payload;
     },
   },
 });
 
 export default floorSlice.reducer;
-export const {setFloor,changeFloor} = floorSlice.actions
+export const { setFloor, changeFloor } = floorSlice.actions;
