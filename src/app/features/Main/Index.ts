34319f97de2d5fb9a3116e1c43ea72a1559c7746
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-05-20 16:56:26 +0900

    フォルダ構成を変更

--- src/app/features/Main/Index.ts
+++ src/app/features/Main/Index.ts
@@ -3,7 +3,7 @@ import FloorLink from "./components/FloorLink/FloorLink";
 import FloorTable from "./components/FloorTable/FloorTable";
 import RoomModal from "./components/Modal/RoomModal/RoomModal";
 import TaskModal from "./components/Modal/TaskModal/TaskModal";
-import Rooms from "./components/Rooms/Rooms";
+import Rooms from "./components/FloorTable/Rooms/Rooms";
 import SideBar from "./components/SideBar/SideBar";
 import StateSign from "./components/StateSign/StateSign";
 
