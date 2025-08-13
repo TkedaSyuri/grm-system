1c2e1fd188540e8030eb4b03df43ffba276d9d2e
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-08-13 16:51:33 +0900

    フロアのデータ更新を双方向通信で実装

--- src/app/features/hooks/useGetFloor.ts
+++ src/app/features/hooks/useGetFloor.ts
@@ -1,7 +1,8 @@
 import useSWR from "swr";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAppDispatch } from "../Redux/hooks";
 import { setFloor } from "@/app/features/Redux/floor/floorSlice";
+import { io, Socket } from "socket.io-client";
 
 async function fetcher(key: string) {
   return fetch(key).then((res) => res.json());
@@ -9,22 +10,45 @@ async function fetcher(key: string) {
 
 export const useGetFloor = (floorNumber: string) => {
   const dispatch = useAppDispatch();
-  const { data, isLoading } = useSWR(
+  const socketRef = useRef<Socket | null>(null);
+  const { data, mutate, isLoading } = useSWR(
     `${process.env.NEXT_PUBLIC_API_BASEURL}/api/room/get/floor/${floorNumber}`,
     fetcher,
-    {
-      refreshInterval: 3000,
-    }
+    {revalidateOnFocus: false}
   );
 
-  useEffect(() => {
-    if (data) {
-      dispatch(setFloor(data));
-    }
-  }, [dispatch, data]);
 
-  return {
-    data,
-    isLoading,
-  };
+useEffect(() => {
+    if (data) {
+      dispatch(setFloor(data));  
+    }
+  }, [data, dispatch]);
+
+
+  useEffect(() => {
+    // Socket.IO接続開始
+    socketRef.current = io(process.env.NEXT_PUBLIC_API_BASEURL || "");
+
+    socketRef.current.on("connect", () => {
+      console.log("Socket connected:", socketRef.current?.id);
+    });
+
+    // サーバーからの更新通知受信
+    socketRef.current.on("roomStateUpdated", (msg) => {
+      console.log("roomStateUpdated received:", msg);
+
+      // データを再取得してReduxを更新
+      mutate();
+    });
+
+    socketRef.current.on("disconnect", () => {
+      console.log("Socket disconnected");
+    });
+
+    return () => {
+      socketRef.current?.disconnect();
+    };
+  }, [mutate]);
+
+  return { data, isLoading };
 };
